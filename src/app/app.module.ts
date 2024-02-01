6df532c87ca36dc10c2fdc40a2aa7783bd62feae
Alex <alex@google.com>
2024-02-02 00:00:42 +0300

    completed beginners task (without NGRX), enabled interaction with local storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersListComponent } from './components/users-list/users-list.component';
 import { UserCardComponent } from './components/user-card/user-card.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateEditUserComponent } from './components/create-edit-user/create-edit-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersListComponent,
-    UserCardComponent
+    UserCardComponent,
+    CreateEditUserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
